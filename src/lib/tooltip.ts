2b2b07c8c1063cb79c76ebd70cc61228ac5d9869
Jason Holtzen <mystikaljester@gmail.com>
2023-07-14 11:12:11 -0500

    removed svelte-tippy

--- src/lib/tooltip.ts
+++
@@ -1,9 +0,0 @@
-import 'tippy.js/dist/tippy.css';
-import 'tippy.js/themes/material.css';
-import { createTippy } from 'svelte-tippy';
-
-export const tooltip = createTippy({
-	arrow: false,
-	placement: 'left',
-	theme: 'material'
-});
